d7ed941572c20ff6c91af1431b6033d1aaf84dc9
JaeYoonJung08 <whitecy01@naver.com>
2024-08-01 20:50:07 +0900

    feat: gitignore 새로운 규칙

---
+++ Source/UnrealClient/Fire.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Fire.h"
+
+
+// Sets default values
+AFire::AFire()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	CO_EmissionRate = 0.1f; // 예시 값, 필요에 따라 조정
+}
+
+// Called when the game starts or when spawned
+void AFire::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFire::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
