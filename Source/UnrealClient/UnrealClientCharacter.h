8361533073b6fd0d4cae573417bb1694540e30af
JaeYoonJung08 <whitecy01@naver.com>
2024-08-13 20:34:08 +0900

    feat: Voxel 카메라 2D 구현

--- Source/UnrealClient/UnrealClientCharacter.h
+++ Source/UnrealClient/UnrealClientCharacter.h
@@ -5,7 +5,10 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+#include "Voxel_one.h"
 #include "UnrealClientCharacter.generated.h"
+
+
 
 class UInputComponent;
 class USkeletalMeshComponent;
@@ -43,6 +46,8 @@ public:
 protected:
 	virtual void BeginPlay();
 
+	virtual void Tick(float DeltaTime) override; // Tick 함수 오버라이드
+
 public:
 		
 	/** Look Input Action */
@@ -66,6 +71,31 @@ public:
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	////Voxel_one Voxel을 생성하고 관리할 수 있는 변수 추가
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Voxel")
+	//TSubclassOf<AVoxel_one> VoxelClass;
+
+private:
+	// 스폰된 Voxel을 관리하는 포인터
+	//AVoxel_one* SpawnedVoxel;
+	TArray<AVoxel_one*> SpawnedVoxels;  // 생성된 복셀들을 저장하는 배열
+	//int32 GridSize = 15;  // 15x15 그리드
+	int32 GridSizeY = 23;  // Y축 방향의 그리드 크기
+	int32 GridSizeZ = 15;  // Z축 방향의 그리드 크기
+
+
+
+	//float VoxelSpacing = 50.0f;  // 복셀 간의 간격
+	float DistanceFromCamera = 200.0f; // 카메라에서 그리드까지의 거리
+	//FVector VoxelSize = FVector(100.0f, 100.0f, 100.0f);  // 복셀 크기
+	FVector VoxelSize;
+	int32 NumX;
+	int32 NumY;
+	int32 NumZ;
+
+	// Set default voxel size
+	//VoxelSize = FVector(14.58125f, 14.9976f, 15.2614f);
 
 };
 
